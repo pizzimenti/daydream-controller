1eaccd1a2ee678786b0864e0d07e34448ec17e91
Brad Pizzimenti <brad@gennakersystems.com>
2017-05-24 09:28:28 -0700

    add ability to spawn arrows by calling a method of GvrController

--- Assets/Scripts/Quiver.cs
+++ Assets/Scripts/Quiver.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Quiver : MonoBehaviour {
+
+    public GameObject arrowPrefab;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,17 @@ public class Quiver : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        // listen for click events
+        if (GvrController.ClickButtonDown)
+        {
+            CreateArrow();
+        }
 		
 	}
+
+    private void CreateArrow()
+    {
+        Instantiate(arrowPrefab);
+    }
 }
